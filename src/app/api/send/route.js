e0a4b138082e3bbd915e79f238405921079f15fd
Ankit Kumawat <ankitkumawat1508@gmail.com>
2024-12-11 00:36:53 +0530

    changed route

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,32 +1,28 @@
 import { NextResponse } from "next/server";
-import { resend } from "@/lib/resend";
+import { Resend } from "resend";
 
-export async function handler(req, res) {
-  if(req.method == 'POST'){
-   const { name, email, message } = await req.json();
-  
+const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
+
+export async function POST(req, res) {
+  const { email, subject, message } = await req.json();
+  console.log(email, subject, message);
   try {
     const data = await resend.emails.send({
-      from: 'onboarding@resend.dev' ,
-      to: email,
-      subject: `New Message from ${name}`,
-      html :`
-        <p>You have a new message from your portfolio contact form:</p>
-          <p><strong>Name:</strong> ${name}</p>
-          <p><strong>Email:</strong> ${email}</p>
-          <p><strong>Message:</strong></p>
-          <p>${message}</p>
-      `,
+      from: fromEmail,
+      to: [fromEmail, email],
+      subject: subject,
+      react: (
+        <>
+          <h1>{subject}</h1>
+          <p>Thank you for contacting us!</p>
+          <p>New message submitted:</p>
+          <p>{message}</p>
+        </>
+      ),
     });
     return NextResponse.json(data);
   } catch (error) {
-    console.error('Error sending email with Resend', error);
     return NextResponse.json({ error });
   }
-} else {
-  res.setHeader('Allow', ['POST']);
-  res.status(405).end(`Method ${req.method} Not Allowed`);
 }
-}
-
-
--- src/helpers/sendVerificationEmail.ts
+++
@@ -1,2 +0,0 @@
-import { resend } from "@/lib/resend";
-
